1bbb6f3747bee3a2ce1d09195285dbc6c3af6188
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2025-11-11 18:16:36 +0000

    feat(integrations): add github.d.ts

---
+++ growth-agent/src/integrations/github.d.ts
@@ -0,0 +1,4 @@
+import { Octokit } from '@octokit/rest';
+export declare function getOctokit(): Promise<Octokit>;
+export declare function getGitAuthToken(): Promise<string>;
+//# sourceMappingURL=github.d.ts.map
