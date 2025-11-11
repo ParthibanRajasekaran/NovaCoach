96e504f9cbf33bf724cb04fd4efdef82833aa38b
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2025-11-11 18:43:42 +0000

    Revert "feat(runners): add types.js"
    This reverts commit 7e7e11ff09405f95d673d9becb643ea93fb517d7.

--- growth-agent/src/runners/types.js
+++
@@ -1,9 +0,0 @@
-export function createLogger(verbose) {
-    return {
-        info: console.log.bind(console),
-        warn: console.warn.bind(console),
-        error: console.error.bind(console),
-        debug: verbose ? console.debug.bind(console) : () => { },
-    };
-}
-//# sourceMappingURL=types.js.map
