75e423a01b96415031effee72403f41e6a366b4e
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2025-11-11 18:16:55 +0000

    feat(runners): add pr.d.ts

---
+++ growth-agent/src/runners/pr.d.ts
@@ -0,0 +1,8 @@
+export type PrOptions = {
+    locale: string;
+    repo?: string | undefined;
+    base?: string | undefined;
+    contentDir?: string | undefined;
+};
+export declare function run({ locale, repo, base, contentDir, }: PrOptions): Promise<void>;
+//# sourceMappingURL=pr.d.ts.map
